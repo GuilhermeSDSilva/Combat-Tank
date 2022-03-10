b9d9e501cd2f48fb91692c7d62e2de86c35bc751
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-10 13:06:09 -0300

    Update wall.py

--- wall.py
+++ wall.py
@@ -1,1 +1,9 @@
+import pygame
+from config import *
+def wall(screen):
+    wall_up = pygame.draw.rect(screen, WALL_COLOR, (WALL_UP_POSITION,WALL_UP_SIZE))
+    wall_down = pygame.draw.rect(screen, WALL_COLOR, (WALL_DOWN_POSITION,WALL_UP_SIZE))
+    wall_right = pygame.draw.rect(screen, WALL_COLOR, (WALL_RIGHT_POSITION,WALL_RIGHT_SIZE))
+    wall_left = pygame.draw.rect(screen, WALL_COLOR, (WALL_LEFT_POSITION,WALL_RIGHT_SIZE))
+                                
 
