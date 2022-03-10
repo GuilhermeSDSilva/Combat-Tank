c28fb4fce868fbd26ee20e29cc808df274c18caa
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-10 13:07:42 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
+from config import *
+from wall import *
+import sys
+import pygame
+pygame.init()
+SCREEN = pygame.display.set_mode(SCREEN_SIZE)
+while True:
+    SCREEN.fill(SCREEN_COLOR)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+    wall(SCREEN)
+    
+    pygame.display.flip()
+pygame.quit()
 
