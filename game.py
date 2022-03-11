466ea9ec1d3f0a9ebeaf61ed7ee3c9f9ee728346
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-11 12:42:21 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,26 @@
 from config import *
 from wall import *
+from screen import *
+from player import *
 import sys
 import pygame
-pygame.init()
-SCREEN = pygame.display.set_mode(SCREEN_SIZE)
-while True:
-    SCREEN.fill(SCREEN_COLOR)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-    draw_wall(SCREEN)
-    
-    pygame.display.flip()
-pygame.quit()
 
+
+def game_loop():
+    pygame.init()
+    #desnhar tela( está dentro de screen)
+    SCREEN = screen
+    
+    while True:
+        SCREEN.fill(SCREEN_COLOR)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                
+        #desenhar paredes (está dentro de wall)      
+        draw_wall(SCREEN)
+        
+        SCREEN.blit(personagem, (350, 350))
+        
+        pygame.display.flip()
+    pygame.quit()
