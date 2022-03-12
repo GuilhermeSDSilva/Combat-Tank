b781cbd8f97e7f46362d3fdb5dcb246975e64096
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-11 21:29:22 -0300

    Update wall.py

--- wall.py
+++ wall.py
@@ -5,5 +5,4 @@ def draw_wall(screen):
     wall_down = pygame.draw.rect(screen, WALL_COLOR, (WALL_DOWN_POSITION,WALL_UP_SIZE))
     wall_right = pygame.draw.rect(screen, WALL_COLOR, (WALL_RIGHT_POSITION,WALL_RIGHT_SIZE))
     wall_left = pygame.draw.rect(screen, WALL_COLOR, (WALL_LEFT_POSITION,WALL_RIGHT_SIZE))
-                                
-
+    
