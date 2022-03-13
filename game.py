ec2a293fa15a8fbc800c61d6dbf4030a155ec90d
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-13 17:28:01 -0300

    Update game.py

--- game.py
+++ game.py
@@ -3,12 +3,15 @@ from config import *
 from wall import *
 from screen import *
 from barrier import *
-
+from bullet import *
 
 import sys
 import pygame
 
 def game_loop():
+
+    
+        
     pygame.init()
     #desnhar tela( está dentro de screen)
     SCREEN = screen
@@ -22,11 +25,16 @@ def game_loop():
     player_x = 100
     player_y = 390
     marcador = 12
-
-    
-    
+    bala_movimento = False
+    bala_direcao = marcador
     W_C = Wall_Collision()
     B_C = Barrier_Collision()
+    B = Bullet()
+    bala = B._init_()
+    
+    bala_x = player_x
+    bala_y = player_y
+
     while True:
         SCREEN.fill(SCREEN_COLOR)
         for event in pygame.event.get():
@@ -47,6 +55,9 @@ def game_loop():
                         marcador -=11
                     else:
                         marcador +=1
+                if event.key == pygame.K_z:
+                    bala_movimento = True
+                
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_w:
                     frente = False
@@ -107,7 +118,7 @@ def game_loop():
         #desenhar paredes (está dentro de wall)      
         draw_wall(SCREEN)
         SCREEN.blit(player, (player_x, player_y))
-        
+        SCREEN.blit(bala, (bala_x, bala_y))
         SCREEN.blit(barreira1, (120, 330))
         SCREEN.blit(barreira2, (290, 350))
         SCREEN.blit(barreira3, (450, 200))
@@ -152,10 +163,23 @@ def game_loop():
         #chamando a colisão com as paredes
         player_x = W_C.d_e(player_x)
         player_y = W_C.c_b(player_y)
-
+        
         #chamando as colisões com as barreiras
         player_x = B_C.direita_esquerda(player_x,player_y)
         player_y = B_C.cima_baixo(player_x,player_y)
-        
+
+        if bala_movimento == True:
+            bala_direcao = bala_direcao
+            bala_x = B.moviment_x(bala_x, bala_movimento,bala_direcao)
+            bala_y = B.moviment_y(bala_y, bala_movimento,bala_direcao)
+
+        elif bala_movimento == False:
+            bala_direcao = marcador
+            bala_x = B.position_stopx(marcador, player_x, player_y)
+            bala_y = B.position_stopy(marcador, player_x, player_y)
+
+        if (bala_y <=100) and (bala_movimento == True):
+            bala_movimento = False
+            
         pygame.display.flip()
     pygame.quit()
