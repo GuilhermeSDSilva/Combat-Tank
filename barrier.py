e8376304814726a02ee4a7013939e8d8b1222b07
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-13 12:49:44 -0300

    Update barrier.py

--- barrier.py
+++ barrier.py
@@ -22,7 +22,6 @@ barreira6 = pygame.transform.rotate(barreira6, 180)
 barreira6  = pygame.transform.scale(barreira6, BARR1_SIZE)
 
 class Barrier_Collision:
-    #todas as colisões pela esquerda
     def direita_esquerda(self, pos_x,pos_y):
         #barreira 1
         if (170 <= pos_x <=173) and (298 <= pos_y <= 480):
@@ -60,7 +59,7 @@ class Barrier_Collision:
             self.ant_x = pos_x +1
             return self.ant_x
         #barreira 6
-        elif (724 <= pos_x <=727) and (298 <= pos_y <= 480):
+        elif (720 <= pos_x <=723) and (298 <= pos_y <= 480):
             self.ant_x = pos_x -1
             return self.ant_x
         elif (770 <= pos_x <=773) and (298 <= pos_y <= 480):
@@ -69,3 +68,51 @@ class Barrier_Collision:
         else:
             self.ant_x = pos_x
             return self.ant_x
+
+    def cima_baixo(self, pos_x,pos_y):
+        #barreira 1
+        if (155 <= pos_x <=228) and (298 <= pos_y <= 301):
+            self.ant_y = pos_y -1
+            return self.ant_y
+        elif (155 <= pos_x <=228) and (477 <= pos_y <= 480):
+            self.ant_y = pos_y +1
+            return self.ant_y
+        #barreira 2
+        elif (254 <= pos_x <=369) and (355 <= pos_y <= 358):
+            self.ant_y = pos_y -1
+            return self.ant_y
+        elif (254 <= pos_x <=369) and (420 <= pos_y <= 423):
+            self.ant_y = pos_y +1
+            return self.ant_y
+        #barreira 3
+        elif (414 <= pos_x <=528) and (205 <= pos_y <= 208):
+            self.ant_y = pos_y -1
+            return self.ant_y
+        elif (414 <= pos_x <=528) and (270 <= pos_y <= 273):
+            self.ant_y = pos_y +1
+            return self.ant_y
+        #barreira 4
+        elif (414 <= pos_x <=528) and (500 <= pos_y <= 503):
+            self.ant_y = pos_y -1
+            return self.ant_y
+        elif (414 <= pos_x <=528) and (570 <= pos_y <= 573):
+            self.ant_y = pos_y +1
+            return self.ant_y
+        # barreira 5
+        elif (564 <= pos_x <=679) and (350 <= pos_y <= 353):
+            self.ant_y = pos_y -1
+            return self.ant_y
+        elif (564 <= pos_x <=679) and (420 <= pos_y <= 423):
+            self.ant_y = pos_y +1
+            return self.ant_y
+        # barreira 6
+        elif (709 <= pos_x <=782) and (298 <= pos_y <= 301):
+            self.ant_y = pos_y -1
+            return self.ant_y
+        elif (709 <= pos_x <=782) and (477 <= pos_y <= 480):
+            self.ant_y = pos_y +1
+            return self.ant_y
+        else:
+            self.ant_y = pos_y
+            return self.ant_y
+        
