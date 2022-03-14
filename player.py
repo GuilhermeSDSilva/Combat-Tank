6de35762bacfdf9121fa559b9cc9efb9b9467026
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-13 21:26:09 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,64 @@
 import pygame
+
+class Personagem:
+    def sprites(self, m):
+        if m == 12:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            return self.p
+        elif m == 1:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, -30)
+            return self.p
+        elif m == 2:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, -60)
+            return self.p
+        elif m == 3:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, -90)
+            return self.p
+        elif m == 4:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, -120)
+            return self.p
+        elif m == 5:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, -150)
+            return self.p
+        elif m == 6:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, -180)
+            return self.p
+        elif m == 7:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, 150)
+            return self.p
+        elif m == 8:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, 120)
+            return self.p
+        elif m == 9:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, 90)
+            return self.p
+        elif m == 10:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, 60)
+            return self.p
+        elif m ==11:
+            self.p = pygame.image.load("img/tank(1).png")
+            self.p = pygame.transform.scale(self.p, (40, 40))
+            self.p = pygame.transform.rotate(self.p, 30)
+            return self.p
+
