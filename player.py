f969a67c3be11b097c2e8d8f73d1c176718bb886
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-14 15:02:38 -0300

    Update player.py

--- player.py
+++ player.py
@@ -62,3 +62,78 @@ class Personagem:
             self.p = pygame.transform.rotate(self.p, 30)
             return self.p
 
+    def movimentox_frente(self,posx,marc):
+        if marc == 3:
+            self.posi_x = posx + 1
+            return self.posi_x
+        elif marc == 4:
+            self.posi_x = posx + 1
+            return self.posi_x
+        elif marc == 5:
+            self.posi_x = posx + 0.5
+            return self.posi_x
+        elif marc == 6:
+            self.posi_x = posx
+            return self.posi_x
+        elif marc == 7:
+            self.posi_x = posx - 0.5
+            return self.posi_x
+        elif marc == 8:
+            self.posi_x = posx - 1
+            return self.posi_x
+        elif marc == 9:
+            self.posi_x = posx - 1
+            return self.posi_x
+        elif marc == 10:
+            self.posi_x = posx - 1
+            return self.posi_x
+        elif marc == 11:
+            self.posi_x = posx - 0.5
+            return self.posi_x
+        elif marc == 12:
+            self.posi_x = posx
+            return self.posi_x
+        elif marc == 1:
+            self.posi_x = posx + 0.5
+            return self.posi_x
+        elif marc == 2:
+            self.posi_x = posx + 1
+            return self.posi_x                
+    def movimentoy_frente(self,posy,marc):
+        if marc == 3:
+            self.posi_y = posy
+            return self.posi_y
+        elif marc == 4:
+            self.posi_y = posy + 0.5
+            return self.posi_y
+        elif marc == 5:
+            self.posi_y = posy + 1
+            return self.posi_y
+        elif marc == 6:
+            self.posi_y = posy + 1
+            return self.posi_y
+        elif marc == 7:
+            self.posi_y = posy + 1
+            return self.posi_y
+        elif marc == 8:
+            self.posi_y = posy + 0.5
+            return self.posi_y
+        elif marc == 9:
+            self.posi_y = posy
+            return self.posi_y
+        elif marc == 10:
+            self.posi_y = posy - 0.5
+            return self.posi_y
+        elif marc == 11:
+            self.posi_y = posy - 1
+            return self.posi_y
+        elif marc == 12:
+            self.posi_y = posy - 1
+            return self.posi_y
+        elif marc == 1:
+            self.posi_y = posy - 1
+            return self.posi_y
+        elif marc == 2:
+            self.posi_y = posy - 0.5
+            return self.posi_y
+
