dfb1bd3847b6dd6e81513eddf54a9dd391fbf9a4
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-03-28 17:55:33 -0300

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -1,9 +1,12 @@
 import pygame
+
+
 class SONS:
     def tiro(self):
         self.som_tiro = pygame.mixer.Sound("sound/shoot.wav")
         self.som = self.som_tiro.play()
         return self.som
+
     def dest(self):
         self.som_destruct = pygame.mixer.Sound("sound/destruct.wav")
         self.som = self.som_destruct.play()
